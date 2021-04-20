c5ca3517da53cf5508aa760fe9f3f65d100aa80a
nightsky98525-dev <nightsky98525@gmail.com>
2021-04-20 19:15:27 +0800

    add header and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { MatInputModule } from "@angular/material/input";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, MatInputModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, HeaderComponent, SidebarComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
