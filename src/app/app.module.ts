f236b8f33cc2ae2e81b80261e4bbc72cd0e1984b
nightsky98525-dev <nightsky98525@gmail.com>
2021-04-20 19:36:52 +0800

    add mat-tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { MatInputModule } from "@angular/material/input";
+import { MatTabsModule } from "@angular/material/tabs";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from "./header/header.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatInputModule],
-  declarations: [AppComponent, HelloComponent, HeaderComponent, SidebarComponent],
+  imports: [BrowserModule, FormsModule, MatTabsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
