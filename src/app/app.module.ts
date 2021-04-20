9dc0db13609d2dfb01bfb2d1ec717b83957de713
nightsky98525-dev <nightsky98525@gmail.com>
2021-04-21 05:10:59 +0800

    create service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,27 @@ import { MatTabsModule } from "@angular/material/tabs";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 import { HeaderComponent } from "./header/header.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
+import { TemplateComponent } from "./sidebar/template/template.component";
+import { ContentComponent } from "./sidebar/content/content.component";
+import { OptionsComponent } from "./sidebar/options/options.component";
+import { DesignComponent } from "./sidebar/design/design.component";
+import { SideshowPanelComponent } from "./sideshow-panel/sideshow-panel.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, MatTabsModule, BrowserAnimationsModule],
   declarations: [
     AppComponent,
-    HelloComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    TemplateComponent,
+    ContentComponent,
+    OptionsComponent,
+    DesignComponent,
+    SideshowPanelComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: []
 })
 export class AppModule {}
---
+++ src/app/sideshow-panel/sideshow-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sideshow-panel',
+  templateUrl: './sideshow-panel.component.html',
+  styleUrls: ['./sideshow-panel.component.css']
+})
+export class SideshowPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
